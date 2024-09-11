b33abf9a222ecb522e7a864c82c8a12a3a0098cf
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-09-11 14:28:09 +0000

    add welcome message and an intruduction about the game and an input for the player name

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #importing math module
 import math 
 
+print("\nWelcome to the Apple Park Adventure!")
+print("As a tech enthusiast, you have decided to explore the heart of innovation at Apple Park.")
+print("However, as you wander through the sprawling campus, you realize you're lost.")
+print("You will need to choose your path wisely to make it to the highest office.\n")
+
+player_name = input("Let's start with your name: ")
+print(f"Good luck, {player_name}.\n")
